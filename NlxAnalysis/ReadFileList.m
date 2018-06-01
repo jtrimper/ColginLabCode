833236b263dfffc362125a104cc4fa21886965d9
jtrimper <jbtrimper@gmail.com>
2018-06-01 14:47:20 -0500

    Added Nlx Code

---
+++ NlxAnalysis/ReadFileList.m
@@ -0,0 +1,33 @@
+function F = ReadFileList(fn)
+
+% F = ReadFileList(fn)
+%
+% INPUTS: 
+%   fn -- an ascii file of filenames, 1 filename per line
+% 
+% OUTPUTS:
+%   F -- a cell array of filenames suitable for use in programs
+%        such as LoadSpikes
+%
+% Now can handle files with headers
+
+% ADR 1998
+% version L4.1
+% status: PROMOTED
+
+% v4.1 added ReadHeader
+
+[fp,errmsg] = fopen(fn, 'rt');
+if (fp == -1)
+   error(['Could not open "', fn, '". ', errmsg]);
+end
+
+ReadHeader(fp);
+ifp = 1;
+while (~feof(fp))
+   F{ifp} = fgetl(fp);
+   ifp = ifp+1;
+end
+fclose(fp);
+
+F = F';
